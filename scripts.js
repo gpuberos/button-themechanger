804ea1a7f169b3fb353e6b9d771f5187a32db61b
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2023-12-06 16:08:22 +0100

    fix: remove scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Function Theme Button
-// Ajoute la class .customtheme dans le body
-function themeButton() {
-    document.body.classList.toggle("customtheme");
-}
-
-// Lorsqu'on clique sur le button qui a la classe .button, lancer la fonction themeButton()
-document.querySelector(".button").addEventListener("click", themeButton);
